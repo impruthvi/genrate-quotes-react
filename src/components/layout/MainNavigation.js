a40ac616382fc3ed3c418d3242d591524b9c6d54
Pruthvisinh Rajput <pruthvisinh.rajput@yudiz.com>
2022-09-02 09:56:08 +0530

    Createda dynamic link for Quote details page

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -4,7 +4,7 @@ import classes from "./MainNavigation.module.css";
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}></div>
+      <div className={classes.logo}>Genrate Quotes</div>
       <nav className={classes.nav}>
         <ul>
           <li>
